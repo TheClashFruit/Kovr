810473b358a0be336f7d93f1ad537d9aef9f2451
TheClashFruit <clash.fruit123@gmail.com>
2025-04-05 18:05:46 +0200

    feat: setup project

---
+++ src/main/java/me/theclashfruit/kovr/Kovr.java
@@ -0,0 +1,12 @@
+package me.theclashfruit.kovr;
+
+import net.fabricmc.api.ModInitializer;
+import org.intellij.lang.annotations.Identifier;
+import org.slf4j.Logger;
+
+public class Kovr implements ModInitializer {
+    public static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger("Kovr");
+
+    @Override
+    public void onInitialize() {}
+}
---
+++ src/main/java/me/theclashfruit/kovr/client/KovrClient.java
@@ -0,0 +1,10 @@
+package me.theclashfruit.kovr.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KovrClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
